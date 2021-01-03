2452a3938c085438f992feca15d42c6d3de06891
kennyhcy <35054956+kennyhcy@users.noreply.github.com>
2021-01-03 20:32:51 +0800

    Update WuxiaRPG.py

--- WuxiaRPG.py
+++ WuxiaRPG.py
@@ -3,6 +3,11 @@ class WuxiaRPG(object):
 	"""docstring for WuxiaRPG"""
 	def __init__(self):
 		super(WuxiaRPG, self).__init__()
+		pygame.init()
+		self.screen = pygame.display.set_mode((1024,768))
+		pygame.display.set_caption("WuxiaRPG")
+		self.bg = pygame.image.load("Data/Player/0.png")
 
-wuxiaRPG = WuxiaRPG()
+if __name__ == '__main__':
+	wuxiaRPG = WuxiaRPG()
 
